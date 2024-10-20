9726675e7d39af1d67bc2050a9e6a3c3666398d1
Ivan Leo <ivanleomk@gmail.com>
2024-10-21 00:27:26 +0800

    feat: added schema validation on the frontend

---
+++ frontend/src/lib/server.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { z } from 'zod';
+import { Clerk } from '@clerk/clerk-js';
+
+// Create an axios instance
+export const api: AxiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:5000', // Adjust this to match your API base URL
+  timeout: 10000, // Set a reasonable timeout
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const clerk = new Clerk( import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
+await clerk.load()
+
+export const makePostRequest = async <T extends z.ZodType<any, any>>(url: string, data: any, schema: T) => {
+    const session = clerk.user
+    const token = await clerk.session?.getToken()
+    if(!token) {
+        throw new Error('No token found')
+    }
+    const response = await api.post(url, data, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+    return schema.parse(JSON.parse(response.data))
+}
