429f0138247d244d7d4f10771d5ed77cde284450
Ivan Leo <ivanleomk@gmail.com>
2024-10-24 15:02:25 +0800

    fix: fix type errors and remove StoryForm component

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,21 +1,6 @@
-import EmptyState from "@/components/EmptyState";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { cn } from "@/lib/utils";
-import { PlusCircleIcon } from "lucide-react";
-import React, { useState } from "react";
-import Story from "./story";
+import EmptyState from "@/components/emptystate";
+import { useState } from "react";
 import StoryDialog from "@/components/storydialog";
-
-// import { Link } from "react-router-dom";
 
 const Home = () => {
   const [open, setOpen] = useState(false);
