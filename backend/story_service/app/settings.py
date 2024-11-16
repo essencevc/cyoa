44f3d52696083a64ca9da539d1e621f786a12faf
Ivan Leo <ivanleomk@gmail.com>
2024-11-16 23:11:03 +0800

    fix: migrated the settings in story service to use openai api key

--- backend/story_service/app/settings.py
+++ backend/story_service/app/settings.py
@@ -2,7 +2,7 @@ from common.settings import CoreSettings
 
 
 class Settings(CoreSettings):
-    GOOGLE_API_KEY: str
+    OPENAI_API_KEY: str
 
     class Config:
         env_file = "./story_service/.env"
