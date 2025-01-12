66e17f8a17f9e3bb65f7cfdb81f985d9006427e6
Ivan Leo <ivanleomk@gmail.com>
2025-01-13 01:13:44 +0800

    fix: updated README

--- restate/helpers/env.py
+++ restate/helpers/env.py
@@ -5,10 +5,7 @@ class Env(BaseSettings):
     GOOGLE_API_KEY: str
     DB_URL: str
     DB_TOKEN: str
-    AUDIO_ENDPOINT: str
     IMAGE_ENDPOINT: str
-    RESTATE_ENDPOINT: str
-    RESTATE_TOKEN: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_ACCESS_KEY_ID: str
     AWS_REGION: str
